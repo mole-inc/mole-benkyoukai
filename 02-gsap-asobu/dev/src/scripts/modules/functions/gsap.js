06abd012567bf41229a5e20962c1731d6eb11cbc
sengokukun <kizuku.sengokukun@gmail.com>
2020-10-12 20:24:29 +0900

    feat: gsap-de-asobu

--- 02-gsap-asobu/dev/src/scripts/modules/functions/gsap.js
+++ 02-gsap-asobu/dev/src/scripts/modules/functions/gsap.js
@@ -1,4 +1,6 @@
 import MasterController from "../controllers/MasterController"
+import gsap from 'gsap'
+import ScrollTrigger from 'gsap/ScrollTrigger'
 
 class Gsap extends MasterController {
   constructor() {
@@ -7,6 +9,100 @@ class Gsap extends MasterController {
   }
 
   initListeners() {
+    this.yoyo()
+    this.timeline()
+    this.fromTo()
+    this.getProps()
+    this.scroll()
+    this.staggerText()
+  }
+
+  yoyo() {
+    gsap.to('.yoyo-box', {
+      duration: 1,
+      x: 300,
+      yoyo: true,
+      repeat: -1
+    })
+  }
+
+  timeline() {
+    const tl = gsap.timeline({
+      yoyo: true,
+      repeat: -1,
+
+    })
+    tl.to('.timeline', {
+      duration: 1,
+      x: 500,
+    }).to('.timeline', {
+      duration: 1,
+      y: 200
+    })
+  }
+
+  fromTo() {
+    gsap.fromTo('.fromTo', {
+      autoAlpha : 0
+    },
+    {
+      duration : 1,
+      autoAlpha : 0.5,
+      yoyo: true,
+      repeat: -1,
+    })
+  }
+
+  getProps() {
+    const x = gsap.getProperty('.getProps', 'x')
+    gsap.to('.getProps', {
+      duration: 1,
+      x: x + 100
+    })
+  }
+
+  scroll() {
+    gsap.registerPlugin(ScrollTrigger)
+    const tl = gsap.timeline({
+      defaults: {
+        duration: 1
+      },
+      scrollTrigger : {
+        markers: true,
+        trigger: '.scrollTrigger',
+        start: 'top 50%',
+        end: 'bottom top',
+        toggleActions: "restart none none restart",
+        scrub: true
+      }
+    })
+    tl.to('.scrollTarget', {
+      x: 300
+    })
+  }
+
+  staggerText() {
+    gsap.registerPlugin(ScrollTrigger)
+    const tl = gsap.timeline({
+      defaults: {
+        duration: 1,
+      },
+      scrollTrigger : {
+        markers: true,
+        trigger: '.scrollTarget',
+        start: 'top 50%',
+        end: 'bottom top'
+      }
+    })
+
+    tl.staggerFromTo('.staggerTextBody', 2,{
+      opacity : 0,
+      y: 10,
+    }, {
+      opacity : 1,
+      y: 0,
+      ease: "expo.inOut"
+    }, 0.4)
   }
 }
 
