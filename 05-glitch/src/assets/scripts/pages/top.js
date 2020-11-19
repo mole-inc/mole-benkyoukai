0102cae19da6b17e59a8a88dff02f8a110e8d132
sasaki_sd <sasaki.adoreillura.sd@gmail.com>
2020-11-19 20:55:51 +0900

    fix: ノイズをよりよいものに

--- 05-glitch/src/assets/scripts/pages/top.js
+++ 05-glitch/src/assets/scripts/pages/top.js
@@ -6,8 +6,64 @@ import { ShaderMaterial, TextureLoader, Vector2 } from "three";
 const vertexSource = `
 varying vec2 vUv;
 
+uniform float time;
+
+uniform float uPressed;
+
+//https://github.com/ashima/webgl-noise/blob/master/src/noise2D.glsl
+//お作法ここから
+vec3 mod289(vec3 x) {
+  return x - floor(x * (1.0 / 289.0)) * 289.0;
+}
+
+vec2 mod289(vec2 x) {
+  return x - floor(x * (1.0 / 289.0)) * 289.0;
+}
+
+vec3 permute(vec3 x) {
+  return mod289(((x * 34.0) + 1.0) * x);
+}
+
+float random(vec2 v) {
+  const vec4 C = vec4(0.211324865405187, // (3.0-sqrt(3.0))/6.0,
+      0.366025403784439, // 0.5*(sqrt(3.0)-1.0),
+      -0.577350269189626, // -1.0 + 2.0 * C.x,
+      0.024390243902439); // 1.0 / 41.0,
+  vec2 i = floor(v + dot(v, C.yy));
+  vec2 x0 = v - i + dot(i, C.xx);
+
+  vec2 i1;
+  i1 = (x0.x > x0.y) ? vec2(1.0, 0.0) : vec2(0.0, 1.0);
+  vec4 x12 = x0.xyxy + C.xxzz;
+  x12.xy -= i1;
+
+  i = mod289(i);
+  // Avoid truncation effects in permutation,
+  vec3 p = permute(permute(i.y + vec3(0.0, i1.y, 1.0)) + i.x + vec3(0.0, i1.x, 1.0));
+
+  vec3 m = max(0.5 - vec3(dot(x0, x0), dot(x12.xy, x12.xy), dot(x12.zw, x12.zw)), 0.0);
+  m = m * m;
+  m = m * m;
+
+  vec3 x = 2.0 * fract(p * C.www) - 1.0;
+  vec3 h = abs(x) - 0.5;
+  vec3 ox = floor(x + 0.5);
+  vec3 a0 = x - ox;
+
+  m *= 1.79284291400159 - 0.85373472095314 * (a0 * a0 + h * h);
+
+  vec3 g;
+  g.x = a0.x * x0.x + h.x * x0.y;
+  g.yz = a0.yz * x12.xz + h.yz * x12.yw;
+  return 130.0 * dot(m, g);
+}
+//お作法ここまで
+
 void main() {
   vUv = uv;
+
+  float rand = random(vec2(uv.y, 0.0) * fract(time));
+  //vUv.x += rand * uPressed * 0.01;
 
   gl_Position = vec4(position, 1.0);
 }
@@ -18,13 +74,60 @@ varying vec2 vUv;
 
 uniform float time;
 uniform vec2 resolution;
+
+uniform sampler2D image;
 uniform vec2 imageResolution;
-uniform sampler2D uTex;
+
 uniform float uPressed;
 
-float random (vec2 st) {
-  return fract(sin(dot(st.xy, vec2(12.9898,78.233)))* 43758.5453123);
+//https://github.com/ashima/webgl-noise/blob/master/src/noise2D.glsl
+//お作法ここから
+vec3 mod289(vec3 x) {
+  return x - floor(x * (1.0 / 289.0)) * 289.0;
 }
+
+vec2 mod289(vec2 x) {
+  return x - floor(x * (1.0 / 289.0)) * 289.0;
+}
+
+vec3 permute(vec3 x) {
+  return mod289(((x * 34.0) + 1.0) * x);
+}
+
+float random(vec2 v) {
+  const vec4 C = vec4(0.211324865405187, // (3.0-sqrt(3.0))/6.0,
+      0.366025403784439, // 0.5*(sqrt(3.0)-1.0),
+      -0.577350269189626, // -1.0 + 2.0 * C.x,
+      0.024390243902439); // 1.0 / 41.0,
+  vec2 i = floor(v + dot(v, C.yy));
+  vec2 x0 = v - i + dot(i, C.xx);
+
+  vec2 i1;
+  i1 = (x0.x > x0.y) ? vec2(1.0, 0.0) : vec2(0.0, 1.0);
+  vec4 x12 = x0.xyxy + C.xxzz;
+  x12.xy -= i1;
+
+  i = mod289(i);
+  // Avoid truncation effects in permutation,
+  vec3 p = permute(permute(i.y + vec3(0.0, i1.y, 1.0)) + i.x + vec3(0.0, i1.x, 1.0));
+
+  vec3 m = max(0.5 - vec3(dot(x0, x0), dot(x12.xy, x12.xy), dot(x12.zw, x12.zw)), 0.0);
+  m = m * m;
+  m = m * m;
+
+  vec3 x = 2.0 * fract(p * C.www) - 1.0;
+  vec3 h = abs(x) - 0.5;
+  vec3 ox = floor(x + 0.5);
+  vec3 a0 = x - ox;
+
+  m *= 1.79284291400159 - 0.85373472095314 * (a0 * a0 + h * h);
+
+  vec3 g;
+  g.x = a0.x * x0.x + h.x * x0.y;
+  g.yz = a0.yz * x12.xz + h.yz * x12.yw;
+  return 130.0 * dot(m, g);
+}
+//お作法ここまで
 
 void main() {
 
@@ -38,16 +141,16 @@ void main() {
     vUv.y * ratio.y + (1.0 - ratio.y) * 0.5
   );
 
-  float shift = uPressed * 0.01;
+  float y = uv.y + time * 0.05;
+  float shift = uPressed;
 
-  float rand = random(vec2(uv.y, mod(time, 3.0)));
-  if (rand < 0.2) {
-    uv.x += rand * 0.1 * (uPressed * 0.5);
-  }
+  float noise = mod( random( vec2(y, 0.0) ) * mod(sin(time), 0.01), 0.01);
+  noise += mod( random( vec2(y * 10.0, 0.0) ) * mod(cos(time), 0.01), 0.01);
+  uv.x += noise * shift;
 
-  float r = texture2D(uTex, uv + vec2(shift * rand, 0.0)).r;
-  float g = texture2D(uTex, uv).g;
-  float b = texture2D(uTex, uv + vec2(shift * rand, 0.0)).b;
+  float r = texture2D(image, uv).r;
+  float g = texture2D(image, uv).g;
+  float b = texture2D(image, uv).b;
 
   vec3 color = vec3(r, g, b);
   gl_FragColor = vec4(color, 1.0);
@@ -87,7 +190,7 @@ export default async ()=> {
     pointLight.position.set(2, 2, 2);
 
   // texture読み込み
-    const texLoader = (src)=>{
+    const imageLoader = (src)=>{
       return new Promise((resolve)=>{
         new THREE.TextureLoader().load(
           src,
@@ -97,7 +200,8 @@ export default async ()=> {
         )
       })
     }
-    const texture = await texLoader('https://img.huffingtonpost.com/asset/5cb44b32230000a5006db215.jpeg?cache=iRXyzLXcyh&ops=scalefit_630_noupscale')
+    const url = 'https://upload.wikimedia.org/wikipedia/commons/e/ea/Vervet_Monkey_%28Chlorocebus_pygerythrus%29.jpg'
+    const texture = await imageLoader(url)
 
   // geo作成
     const geo = new THREE.PlaneGeometry(1.5, 1.5, 10, 10);
@@ -110,18 +214,17 @@ export default async ()=> {
       resolution: {
         value: new THREE.Vector2(store.windowWidth, store.windowHeight),
       },
+      image: {
+        value: texture,
+      },
+      imageResolution: {
+        value: new THREE.Vector2(texture.image.naturalWidth, texture.image.naturalHeight),
+      },
       uPressed: {
         value: pressed,
       },
       uMouse: {
-        value: new Vector2(0.5, 0.5),
-      },
-      uTex: {
-        value: texture,
-      },
-      uTexResolution: {
-        type: 'v2',
-        value: new THREE.Vector2(2048, 1356),
+        value: new THREE.Vector2(0.5, 0.5),
       },
     }
     const mat = new THREE.ShaderMaterial({
@@ -129,6 +232,8 @@ export default async ()=> {
       vertexShader: vertexSource,
       fragmentShader: fragmentSource,
     });
+
+    //const mat = new THREE
 
   // mesh作成
     const mesh = new THREE.Mesh(geo, mat);
@@ -139,7 +244,8 @@ export default async ()=> {
     const tick = ()=> {
       const sec = performance.now() / 1000;
       uniforms.time.value = sec;
-      uniforms.uPressed.value += (pressed - uniforms.uPressed.value) * 0.1;
+      //uniforms.uPressed.value += (pressed - uniforms.uPressed.value) * 0.1;
+      uniforms.uPressed.value = pressed;
       renderer.render(scene, camera);
       requestAnimationFrame(tick);
     }
